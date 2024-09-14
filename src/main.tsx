146e9dad9ede3a74ddd6c2ebfaa74f7743d8c27f
amir-ramezany <amirmoghadam467@gmail.com>
2024-09-14 21:54:42 +0330

    fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
-// import App from "./App.tsx";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 import "./index.css";
